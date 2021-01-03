3e6901dc8b33c86d88706deacb53d0ffa5990f2e
Song Shao Ting <qwe820610@gmail.com>
2021-01-03 19:30:48 +0800

    feat: add main.hbs & index.hbs

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyparser = require('body-parser')
+
 const app = express()
 const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(bodyparser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-    res.send('123123')
+    res.render('index')
+})
+
+app.post('/', (req, res) => {
+    console.log(req)
 })
 
 app.listen(port, () => {
     console.log(`Express is running on http://localhost:${port}`)
 })
+
 
   
