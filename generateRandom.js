aaaef1c0112cfa72bb6a704850e449b4a79e5200
Song Shao Ting <qwe820610@gmail.com>
2021-01-04 16:40:09 +0800

    feat: modify generateRandom.js

--- generateRandom.js
+++ generateRandom.js
@@ -1,36 +1,31 @@
 
 // 定義隨機亂碼
 function sample (arry) {
-    let index = Math.floor(Math.random() * arry.length)
-    return arry[index]
-  }
+  const index = Math.floor(Math.random() * arry.length)
+  return arry[index]
+}
 
-  function generateRandom() {
+function generateRandom () {
   // 定義短網址亂碼
   const urlFormat = 5
   const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
   const upperCaseLetters = lowerCaseLetters.toUpperCase()
   const numbers = '1234567890'
-
-  //創建一個陣列來存儲用戶拾取的東西
+  // 創建一個陣列來存儲用戶拾取的東西
   let collection = []
 
   collection = collection.concat([...lowerCaseLetters])
   collection = collection.concat([...upperCaseLetters])
   collection = collection.concat([...numbers])
 
-
-  //開始產生亂碼
+  // 開始產生亂碼
   let random = ''
-  for (let i = 0 ; i < urlFormat ; i++) {
+  for (let i = 0; i < urlFormat; i++) {
     random += sample(collection)
   }
 
-
-  //返回生成的密碼
+  // 返回生成的密碼
   return random
+}
 
-  }
-
-  module.exports = generateRandom
-
+module.exports = generateRandom
